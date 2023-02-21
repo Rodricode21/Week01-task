c54b21998f83e3f639ce68eca4c5a2842d5d3959
rodricode22 <124595307+rodricode22@users.noreply.github.com>
2023-02-20 22:16:50 -0600

    second commit

--- src/components/user/Users.tsx
+++ src/components/user/Users.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { useDebounce } from "../../hooks/useDebounce";
-import { useSearchUser } from "../../hooks/useSearchuser";
+import { useSearchUser } from "../../hooks/useSearchUser";
 import type { GithubUser } from "../../types/globalTypes";
 
 import Search from "../search/Search";
@@ -9,15 +9,23 @@ import User from "./User";
 
 const UsersWrapper = styled.div`
   /* TODO: add display flex to split items */
-  width: 70%;
+  width: 30%;
+  max-height: auto;
   padding: 20px;
   box-shadow: 4px 8px 5px 10px rgba(0, 0, 0, 0.2);
   -webkit-box-shadow: 4px 8px 5px 10px rgba(0, 0, 0, 0.2);
   -moz-box-shadow: 4px 8px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
+const Usercontainer = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const UsersTitle = styled.h1`
-  font-weight: 600;
+  font-weight: 400;
   text-align: center;
 `;
 
@@ -26,7 +34,7 @@ const Users = (): JSX.Element => {
 
   // debounce searched nickname so we're not firing the API request on every key press
   const debouncedSearchUser = useDebounce<string>(user, 2000);
-  const { userData, loading } = useSearchUser<GithubUser, boolean>(
+  const { userData, loading, error } = useSearchUser<GithubUser, boolean>(
     debouncedSearchUser
   );
 
@@ -34,18 +42,20 @@ const Users = (): JSX.Element => {
 
   return (
     <UsersWrapper>
-      <UsersTitle>Search by user's login name</UsersTitle>
-      <Search value={user} onValueChange={onChangeHandler} />
-      {userData ? (
-        <User
-          name={userData.name}
-          image={userData.avatar_url}
-          location={userData.location}
-          url={userData.html_url}
-        />
-      ) : (
-        "el usuario no se ha encontrado"
-      )}
+      <Usercontainer>
+        <UsersTitle>Search by user's login name</UsersTitle>
+        <Search value={user} onValueChange={onChangeHandler} />
+        {userData ? (
+          <User
+            name={userData.name}
+            image={userData.avatar_url}
+            location={userData.location}
+            url={userData.html_url}
+          />
+        ) : (
+          !!error && <p>wrong username</p>
+        )}
+      </Usercontainer>
     </UsersWrapper>
   );
 };
--- src/hooks/useSearchUser.ts
+++ src/hooks/useSearchUser.ts
@@ -2,17 +2,30 @@ import { useCallback, useEffect, useState } from "react";
 
 export const useSearchUser = <T, _>(
   nickname: string
-): { userData: null | T; loading: boolean } => {
+): { userData: null | T; loading: boolean; error: unknown } => {
   const [userData, setUserData] = useState<null | T>(null);
   // const [loading, setLoading] = useState<boolean>(false)
   const [loading] = useState<boolean>(false);
+  const [error, setError] = useState<unknown>();
 
   // useCallback is required here so I don't generate a new reference of the function on every render
   // This is the equivalent of defining the function within the useEffect (will also prevent multiple references on every render, only the ones in the dep array)
   const fetchUser = useCallback(async (userSearch: string) => {
-    const res = await fetch(`https://api.github.com/users/${userSearch}`);
-    const currentUser = await res.json();
-    setUserData(currentUser);
+    try {
+      const res = await fetch(`https://api.github.com/users/${userSearch}`);
+      const currentUser = await res.json();
+      setUserData(currentUser);
+
+      if (res.status != 200) {
+        setUserData(null);
+
+        throw Error("wrong username");
+      }
+    } catch (err) {
+      debugger;
+      console.log(err);
+      setError(err);
+    }
   }, []);
 
   useEffect(() => {
@@ -24,5 +37,6 @@ export const useSearchUser = <T, _>(
   return {
     userData,
     loading,
+    error,
   };
 };
